3ddd842f47ba7e847c64d4f73a1d12c32e6021b8
asdFllL <alcoholiday@outlook.com>
2023-04-13 16:11:15 +0800

    增加总清洗选择

--- data_clear_and_analysis/数据清洗汇总.py
+++ data_clear_and_analysis/数据清洗汇总.py
@@ -3,8 +3,15 @@ import numpy as np
 
 data = pd.read_excel(r'D:\git\新建文件夹\swine-data-analysis\data_clear_and_analysis\database\Clear_v1.0.xlsm', sheet_name='workspace')
 
+columnsList = data.columns.to_list()
+columnsList.drop('日期')
 
+dataNF = pd.DataFrame(data[['日期', '能繁存栏（国家统计局）']])
+dataNF = pd.DataFrame(dataNF.set_index('日期'))
+dataNF = dataNF.resample('M').mean()
 
-dataUse = data.resample('M', on='日期').mean()
+dataZJ = pd.DataFrame(data[['日期', '猪价']])
+dataZJ = pd.DataFrame(dataZJ.set_index('日期'))
+dataZJ = dataZJ.resample('M').mean()
 
-dataUse.head(20)
+data1 = dataZJ.join(dataNF, how='outer')
